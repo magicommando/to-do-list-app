2a4f3925bb86c58bfc46ef3ffc5311bda972da9b
magicommando <magi696king@gmail.com>
2026-04-20 03:04:01 -0600

    updated

--- script.js
+++ script.js
@@ -47,26 +47,6 @@ function renderTasks() {
     });
 }
 
-
-
-
-function renderTasks() {
-    list.innerHTML = "";
-
-    tasks.forEach(task => {
-        const li = document.createElement('li');
-        li.textContent = task.text;
-
-        if (task.completed) {
-            li.classList.add('completed');
-        }
-
-        li.addEventListener('click', () => toggleTask(task.id));
-
-        list.appendChild(li);
-    });
-}
-
 function toggleTask(id) {
     tasks = tasks.map(task =>
         task.id === id ? { ...task, completed: !task.completed } : task
